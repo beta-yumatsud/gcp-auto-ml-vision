43fbdc07201756a45f077e5389858bbdf984e1d9
Beta Yumatsud <hcksx908@yahoo.co.jp>
2020-05-24 14:36:56 +0900

    並行に画像を取得できるように修正

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -1,52 +1,70 @@
-from optparse import OptionParser
+import aiohttp
+import asyncio
 
-import urllib.error
-import urllib.request
+from optparse import OptionParser
 
 image_path = "https://d3304ij6n73kfg.cloudfront.net/prod/thumb/journal_main/{}/500_250"
 
+parallel = 10
+parallel_start = 0
+parallel_end = parallel_start + parallel
 
-def downloader(target_url, dist_path):
+
+async def downloader(target_url, dist_path):
     """
     Download target url and save it to dist path.
     :param target_url:
     :param dist_path:
     :return:
     """
-    try:
-        with urllib.request.urlopen(target_url) as r:
-            image = r.read()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(target_url) as response:
+            image = await response.read()
             with open(dist_path, "wb") as f:
                 f.write(image)
-    except Exception as e:
-        print(e)
-    return
+
+
+async def worker(thread_hold, position, count, target_path):
+    chunk = int(count / parallel)
+    start = position + (thread_hold * chunk)
+    end = start + chunk
+    print("thread_hold: {}, start: {}, end: {}".format(thread_hold, start, end))
+    for i in range(start, end):
+        await downloader(image_path.format(i), target_path.format(i))
 
 
 def main():
     """
-    $ python scripts/download_images.py --dest-path .temp --count=1000
+    $ python scripts/download_images.py --dest-path .temp --position=1 --count=1000
     """
     # command line parameter
-    usage = "usage: %prog --dest-path .temp --count=1000"
+    usage = "usage: %prog --dest-path .temp --position=1 --count=1000"
     parser = OptionParser(usage=usage)
     parser.add_option("-d", "--dest-path", action="store", type="string",
                       dest="dest_path", help="save destination path")
-    parser.add_option("-s", "--start", action="store", type="int",
-                      dest="start", help="start download position")
-    parser.add_option("-e", "--end", action="store", type="int",
-                      dest="end", help="end download position")
+    parser.add_option("-p", "--position", action="store", type="int",
+                      dest="position", help="start download position")
+    parser.add_option("-c", "--count", action="store", type="int",
+                      dest="count", help="download count")
     option, args = parser.parse_args()
     print("dest path: {}".format(option.dest_path))
-    print("start: {}".format(option.start))
-    print("end: {}".format(option.end))
+    print("position: {}".format(option.position))
+    print("count: {}".format(option.count))
+
+    if option.count % parallel == 0:
+        print("OK")
+    else:
+        raise Exception("Invalid count: {}".format(option.count))
+
+    # loop event
+    loop = asyncio.get_event_loop()
 
     # Download
     target_path = option.dest_path + "/sample_{:0=10}.png"
-    for i in range(option.start, option.end):
-        downloader(image_path.format(i), target_path.format(i))
-
-    # TODO DLを並列で回す(async awaitを使ってやってみるのが良き）
+    loop.run_until_complete(asyncio.wait([
+        worker(num, option.position, option.count, target_path) for num in range(parallel_start, parallel_end)
+    ]))
+    loop.close()
 
 
 if __name__ == "__main__":
